3b179e6e5c851d62496ba24aa3705b4025231d85
Jay Leadbetter <jay.leadbetter@gmail.com>
2013-09-07 09:08:31 -0500

    [#20522] adding formset as another level of confirmation of the bug

--- stuff/admin.py
+++ stuff/admin.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib import admin
 from django.core.exceptions import ValidationError
+from django.forms.models import BaseInlineFormSet
 
 from stuff.models import DependentChild, ParentWithDependentChildren
 
@@ -11,15 +12,30 @@ class DependentChildAdminForm(forms.ModelForm):
     """
     def clean(self):
         parent = self.cleaned_data.get('parent')
-        if parent.contingent_field > (self.cleaned_data.get('dependent_field') 
-                                      not in ("Snail Racing", "Underwater Golf")):
+        if parent.contingent_field > 1 and (self.cleaned_data.get('dependent_field') 
+                                            not in ("Snail Racing", "Underwater Golf")):
                 raise ValidationError("You're doing it wrong")
         return super(DependentChildAdminForm, self).clean()
+
+
+class DependentChildInlineFormSet(BaseInlineFormSet):
+    """
+    Alternate way of testing the permissions issue (may be at the
+    formset level and not the form level)
+    """
+    def clean(self):
+        for child_dict in self.cleaned_data:
+            if (self.instance.contingent_field > 1 and 
+                child_dict.get('dependent_field') not in ("Snail Racing", "Underwater Golf")):
+                raise ValidationError("You're doing it wrong")
+        return super(DependentChildInlineFormSet, self).clean()
 
 
 class DependentChildInline(admin.TabularInline):
     model = DependentChild
     form = DependentChildAdminForm
+    # uncomment this to test at the formset level
+    #formset = DependentChildInlineFormSet
 
 
 class ParentWithDependentChildrenAdminForm(forms.ModelForm):
